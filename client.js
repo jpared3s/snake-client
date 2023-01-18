2315e3afedfe587f893099e6ac9d46923f8f3b20
jpared3s <jparedes.wall@gmail.com>
2023-01-18 15:26:58 -0500

    snake game

--- client.js
+++ client.js
@@ -8,15 +8,18 @@ const connect = function () {
     port: 50541,
   });
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding("utf8");// encodes the text in english (think of it as a translator and utf8 as english)
   conn.on("data", (data) => {
     console.log(data)
   });
-  conn.on("connect", () => {
+  conn.on("connect", () => {// upon connection we are sending the server our name JUP and service will console.log the text once connection confirmed
     conn.write("Name: JUP")
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 5000)
     console.log("Successfully connected to game server")
   });
-  
+
   return conn;
 };
 
