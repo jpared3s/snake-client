19919e37aeb728b9829935cc1d0a2de7e588bec2
jpared3s <jparedes.wall@gmail.com>
2023-01-18 14:48:06 -0500

    snake client assignment

--- client.js
+++ client.js
@@ -7,17 +7,16 @@ const connect = function () {
     host: "localhost",
     port: 50541,
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (data) => {
     console.log(data)
   });
   conn.on("connect", () => {
-    console.log("You are connected")
+    conn.write("Name: JUP")
+    console.log("Successfully connected to game server")
   });
-
-
+  
   return conn;
 };
 
